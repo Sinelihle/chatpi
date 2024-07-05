99ff24de56042a48d959092fa80bf3c64a0ac9f3
sinelihle <sinelihle@healthsystems.co.za>
2024-07-05 21:39:20 +0200

    two device chat simulation angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
+   
 export class AppComponent {
-  title = 'chatpi';
+  messages: Message[] = [];
+  newMessage1 = '';
+  newMessage2 = '';
+
+  sendMessage(sender: 'Device 1' | 'Device 2') {
+    const newMessage = sender === 'Device 1' ? this.newMessage1 : this.newMessage2;
+    if (newMessage.trim()) {
+      this.messages.push({
+        text: newMessage,
+        sender: sender,
+        timestamp: new Date()
+      });
+      if (sender === 'Device 1') {
+        this.newMessage1 = '';
+      } else {
+        this.newMessage2 = '';
+      }
+    }
+  }
+}
+
+interface Message {
+  text: string;
+  sender: 'Device 1' | 'Device 2';
+  timestamp: Date;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
